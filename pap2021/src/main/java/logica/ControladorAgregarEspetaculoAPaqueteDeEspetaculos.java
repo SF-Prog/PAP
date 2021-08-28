f7129fd3a7905b8e5c3575724550b0d62b01f289
garciabruno96 <brunogarciaabreu@gmail.com>
2021-08-28 18:07:37 -0300

    Agregar espectaculo a paquete

--- pap2021/src/main/java/logica/ControladorAgregarEspetaculoAPaqueteDeEspetaculos.java
+++ pap2021/src/main/java/logica/ControladorAgregarEspetaculoAPaqueteDeEspetaculos.java
@@ -4,4 +4,39 @@ import interfaces.IControladorAgregarEspetaculoAPaqueteDeEspetaculos;
 
 public class ControladorAgregarEspetaculoAPaqueteDeEspetaculos implements IControladorAgregarEspetaculoAPaqueteDeEspetaculos {
 
+	
+	public ControladorAgregarEspetaculoAPaqueteDeEspetaculos() {
+		super();
+	}
+	@Override
+	public ArrayList<DtPaquete> listarPaquetes() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void seleccionarPaquete(String paquete) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void seleccionarPlataforma(String plataforma) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public ArrayList<DtEspectaculo> listarEspectaculosEnPlataformaNoEnElPaquete() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void seleccionarEspectaculo(String espectaculo) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void ingresarEspectaculoAPaquete() {
+		// TODO Auto-generated method stub
+		
+	}
+}
 }
