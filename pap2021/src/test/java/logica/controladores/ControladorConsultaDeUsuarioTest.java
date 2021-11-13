2a25be6d010a82919d0ec7736690c1d4c3dd9d40
Bruno García <brunogarciaabreu@gmail.com>
2021-11-13 17:35:45 -0300

    Update ControladorConsultaDeUsuarioTest.java

--- pap2021/src/test/java/logica/controladores/ControladorConsultaDeUsuarioTest.java
+++ pap2021/src/test/java/logica/controladores/ControladorConsultaDeUsuarioTest.java
@@ -1,13 +1,44 @@
 package logica.controladores;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.Date;
 
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-public class ControladorConsultaDeUsuarioTest {
 
+import datatypes.DtUsuario;
+import logica.Usuario;
+
+public class ControladorConsultaDeUsuarioTest {
+	ControladorAltaDeUsuario CAU;
+	ControladorConsultaDeUsuario CCU;
+
+	@BeforeEach
+	void inicializacion() {
+		CAU = new ControladorAltaDeUsuario();
+		CCU = new ControladorConsultaDeUsuario();
+	}
+
+	@Test
+	void listarUsuariosTest() {		
+		ArrayList<Usuario> usuarios = CCU.listarUsuarios();
+		assertEquals(usuarios.size(), 0);
+
+		String espectador = "espectadorTest";
+		String emaile = "espectador@test.com";
+		DtUsuario dtEspectador = new DtUsuario(espectador, "nombre", "apellido", emaile, new Date(), "password", "urlImage");
+		CAU.ingresaUsuarioEspectador(dtEspectador);
+
+		usuarios = CCU.listarUsuarios();
+		assertEquals(usuarios.size(), 1);
+		
+		String artista = "artistaTest";
+		String emaila = "artista@test.com";
+		DtUsuario dtArtista = new DtUsuario(artista, "nombre", "apellido", emaila, new Date(), "password", "urlImage");
+		CAU.ingresaUsuarioArtista(dtArtista, "descripcion", "biografia", "link");
+
+		usuarios = CCU.listarUsuarios();
+		assertEquals(usuarios.size(), 2);
+	}
 }
