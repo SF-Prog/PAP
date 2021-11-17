516707c98d40c49ac8128322b6deb75be08e26fc
Bruno García <brunogarciaabreu@gmail.com>
2021-11-17 19:15:40 -0300

    Final

--- pap2021/src/test/java/logica/controladores/ControladorClasesTest.java
+++ pap2021/src/test/java/logica/controladores/ControladorClasesTest.java
@@ -2,16 +2,21 @@ package logica.controladores;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import logica.Artista;
 import logica.Espectaculo;
 import logica.Espectador;
 import logica.Funcion;
 import logica.Paquete;
 import logica.Plataforma;
+import logica.Usuario;
 
 public class ControladorClasesTest {
 	ControladorAltaDeUsuario CAU;
@@ -21,6 +26,23 @@ public class ControladorClasesTest {
 	void inicializacion() {
 		CAU = new ControladorAltaDeUsuario();
 		date = new Date();
+	}
+	
+	@Test
+	void Artista() {
+		Artista a = new Artista();
+		a.setApellido("s");
+		
+		List<Funcion> lfs =  new ArrayList<>();
+		Funcion f1 = new Funcion("nombre funcion", date, "hora", date, "img");
+		lfs.add(f1);
+		a.setFunciones(lfs);
+
+		Funcion f2 = new Funcion("nombre funcion 2", date, "hora", date, "img");
+		a.agregarFuncion(f2);
+		lfs.add(f2);
+		
+		assertEquals(a.getFunciones(), lfs);
 	}
 	
 	@Test
@@ -34,7 +56,18 @@ public class ControladorClasesTest {
 		float costo = 50;
 		String image = "image";
 		
+		Artista artista = new Artista("artista", "nombre", "apellido", "email", date, "password", "urlImage", "descGeneral", "biografia", "link");
+		
+		Funcion f1 = new Funcion("f1", date, "horaInicio", date, "image");
+		List<Funcion> funciones = new ArrayList<Funcion>();
+		funciones.add(f1);
+
+		Funcion f2 = new Funcion("f2", date, "horaInicio", date, "image");
+		
 		Espectaculo e = new Espectaculo(nombre, descripcion, duracion, espectadoresMin, espectadoresMax, url, costo, date, image);
+		e.setArtista(artista);
+		e.setFunciones(funciones);
+		e.addFuncion(f2);
 	
 		assertEquals(nombre, e.getNombre());
 		assertEquals(descripcion, e.getDescripcion());
@@ -45,12 +78,20 @@ public class ControladorClasesTest {
 		assertEquals(costo, e.getCosto());
 		assertEquals(date, e.getFechaRegistro());
 		assertEquals(image, e.getImage());
+		assertEquals(artista, e.getArtista());
+		assertEquals(funciones, e.getFunciones());
 	}
 	
 	@Test
 	void Espectador() {
 		Espectador e = new Espectador("espectadorDSUI4Test", "nombre", "apellido", "espectadorDSUI3@test.com", date, "password", "urlImage");
+		e.setApellido("s");
+		
 		assertEquals(e.getNombre(), "nombre");
+
+		Espectador e2 = new Espectador();
+		e2.setNickName("nickname");
+		assertEquals(e2.getNickName(), "nickname");
 	}
 	
 	@Test
@@ -73,24 +114,98 @@ public class ControladorClasesTest {
 		String nombre = "nombre";
 		String descripcion = "descripcion";
 		float descuento = 0;
-		
+
+		Espectaculo e =new Espectaculo("nombre", "descripcion", 100, 1, 100, "url", 50, date, "image");
+		List<Espectaculo> espectaculos = new ArrayList<Espectaculo>();
+		espectaculos.add(e);
+
 		Paquete p = new Paquete(nombre, descripcion, descuento);
+		p.setEspectaculos(espectaculos);
 	
 		assertEquals(nombre, p.getNombre());
 		assertEquals(descripcion, p.getDescripcion());
 		assertEquals(descuento, p.getDescuento());
+		assertEquals(espectaculos, p.getEspectaculos());
 	}
 	
 	@Test
 	void Plataforma() {
-		String nombre = "nombre";
+		String nombre = "p1";
 		String descripcion = "descripcion";
 		String url = "url";
+
+		Espectaculo e =new Espectaculo("nombre", "descripcion", 100, 1, 100, "url", 50, date, "image");
+		List<Espectaculo> espectaculos = new ArrayList<Espectaculo>();
+		espectaculos.add(e);
 		
-		Plataforma p = new Plataforma(nombre, descripcion, url);
+		Plataforma p1 = new Plataforma(nombre, descripcion, url);
+		p1.setEspectaculos(espectaculos);
 	
-		assertEquals(nombre, p.getNombre());
-		assertEquals(descripcion, p.getDescripcion());
-		assertEquals(url, p.getURLAsociada());
+		assertEquals(nombre, p1.getNombre());
+		assertEquals(descripcion, p1.getDescripcion());
+		assertEquals(url, p1.getURLAsociada());
+		assertEquals(espectaculos, p1.getEspectaculos());
+
+		int duracion = 100;
+		String espectadoresMin = "1";
+		String espectadoresMax = "100";
+		float costo = 50;
+		String fechaAlta = "fechaAlta";
+		nombre = "p2";
+		
+		Plataforma p2 = new Plataforma(nombre, descripcion, duracion, espectadoresMin, espectadoresMax, url, costo, fechaAlta);
+	
+		assertEquals(nombre, p2.getNombre());
+		assertEquals(descripcion, p2.getDescripcion());
+		assertEquals(url, p2.getURLAsociada());
+		assertEquals(duracion, p2.getDuracion());
+		assertEquals(espectadoresMin, p2.getEspectadoresMin());
+		assertEquals(espectadoresMax, p2.getEspectadoresMax());
+		assertEquals(costo, p2.getCosto());
+		assertEquals(fechaAlta, p2.getFechaAlta());
+	}
+	
+	@Test
+	void Usuario() {
+		Usuario u = new Usuario("espectadorDSUI4Test", "nombre", "apellido", "espectadorDSUI3@test.com", date, "password", "urlImage");
+		Usuario u2 = new Usuario("espectadorDSUI4Test2", "nombre2", "apellido2", "espectadorDSUI3@test.com2", date, "password2", "urlImage2");
+		
+		u.getDtUsuario();
+		try {
+			u.seguir(u2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		List<Usuario> lus =  new ArrayList<>();
+		lus.add(u2);
+		u.setUsuariosSeguidos(lus);
+		
+		try {
+			u.seguir(u2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			u.seguir(u2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		u.getUsuariosSeguidos();
+		
+		try {
+			u.dejarDeSeguir(u2);
+		} catch (IOException e) {
+		}
+		
+		try {
+			u.dejarDeSeguir(u2);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(u.getDtUsuario().getNombre(), "nombre");
 	}
 }
